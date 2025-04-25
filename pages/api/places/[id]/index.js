7449f00aa7e33c3f80c0d1be88cc5c2a2f5a336b
jessica <jessy_marian91@hotmail.com>
2025-04-25 12:24:59 +0200

    PUT

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,9 +1,10 @@
+//el back end de POST
 import { places } from "../../../../lib/db.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
+  const place = places.find((place) => place.id === id); //aca Busco el lugar con ese id enplaces. si esta lo asigno a la variable place.
 
   if (!place) {
     response.status(404).json({ status: "Not found" });
@@ -11,4 +12,20 @@ export default function handler(request, response) {
   }
 
   response.status(200).json(place);
+
+  if (request.method === "POST") {
+    // creo el lugar usando los datos enviados en el cuerpo de la solicitud
+    const placeData = request.body;
+
+    try {
+      //se crea un lnuevo lugar
+      await place.create(placeData);
+
+      response.status(201).json({ status: "Place created.", place: placeData });
+    } catch (error) {
+      response.status(500).json({ status: "Error creating place." });
+    }
+    return;
+  }
+  response.status(405).json({ status: "Method Not Allowed" });
 }
--- pages/create.js
+++ pages/create.js
@@ -1,8 +1,8 @@
+//el frntend de POST
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form";
 import { StyledLink } from "../components/StyledLink";
-import useSWR from "swr";
 
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
@@ -11,7 +11,18 @@ const StyledBackLink = styled(StyledLink)`
 export default function CreatePlacePage() {
   const router = useRouter();
   async function addPlace(place) {
-    console.log("adding place");
+    const response = await fetch(`/api/places`, {
+      method: "POST", //  crear un nuevo producto
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(place), // convierte el objeto a JSON
+    });
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.error("error creating places");
+    }
   }
 
   return (
