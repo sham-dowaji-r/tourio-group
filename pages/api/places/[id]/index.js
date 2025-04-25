c7e64265f888991912eca4d11bcd011fb5ca6171
sham dowaji <shamrdowaji@gmail.com>
2025-04-25 11:30:46 +0200

    test the schema

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,17 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "Get") {
+    const place = await Place.findById(id);
+    if (!place) {
+      response.status(404).json({ status: "Not found" });
+      return;
+    }
+    return response.status(200).json(place);
   }
 
-  response.status(200).json(place);
+  response.status(405).json({ status: "Not allowed" });
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,6 +1,12 @@
-import { places } from "../../../lib/db";
+import dbConnect from "@/db/connect";
 
-export default function handler(request, response) {
-  response.status(200).json(places);
-  return;
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    response.status(200).json(places);
+    return;
+  }
+  res.status(405).json({ status: "Not allowed" });
 }
